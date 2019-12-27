173bcdb9910bb1d5c4ae4125970a5f0731a066f1
Brajesh <brajesh.prashant@nobroker.in>
2019-12-27 16:06:49 +0530

    init

---
+++ src/main/java/com/howtodoinjava/demo/swagger/SpringFoxConfig.java
@@ -0,0 +1,41 @@
+package com.howtodoinjava.demo.swagger;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SpringFoxConfig {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo())
+                .useDefaultResponseMessages(false);
+    }
+
+
+    private ApiInfo apiInfo() {
+
+        ApiInfoBuilder apiInfoBuilder = new ApiInfoBuilder();
+        apiInfoBuilder.title("Nobroker HomeService REST API");
+        apiInfoBuilder.description("HomeService");
+        apiInfoBuilder.version("0.0.1-SNAPSHOT");
+        apiInfoBuilder.contact(new Contact("Help", "https://nobroker.in", "help@nobroker.in"));
+        apiInfoBuilder.license("Nobroker LICENSE, Version 0.1");
+        apiInfoBuilder.licenseUrl("https://nobroker.in");
+
+        return apiInfoBuilder.build();
+    }
+}
---
+++ src/test/java/com/howtodoinjava/demo/service/EmployeeServiceTest.java
@@ -0,0 +1,14 @@
+package com.howtodoinjava.demo.service;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class EmployeeServiceTest {
+
+    @Test
+    public void getAllEmployees() {
+        Assert.assertTrue(true);
+    }
+}
